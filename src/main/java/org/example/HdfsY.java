32bd0317ea452fc7e6939f0165df9514037390ab
Yexz <abdelhedi166@gmail.com>
2023-04-02 04:25:29 +0100

    Create works

---
+++ src/main/java/org/example/HdfsY.java
@@ -0,0 +1,159 @@
+package org.example;
+
+import org.apache.commons.io.IOUtils;
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.*;
+import org.apache.hadoop.hdfs.inotify.MissingEventsException;
+import org.jline.utils.Log;
+
+import java.io.*;
+import java.nio.charset.StandardCharsets;
+import java.util.Date;
+import java.util.Map;
+
+public class HdfsY {
+    static FileSystem fileSystem = null;
+    static Configuration config = null;
+
+    public HdfsY() throws IOException {
+        connectHdfs();
+    }
+
+    public static void main(String[] args) throws IOException, MissingEventsException, InterruptedException {
+        HdfsY hdfsY = new HdfsY();
+        try {
+            String filename = "csv989";
+            writeFileToHDFS(filename);
+            addAttr("/user/yexz/"+filename,"name",filename);
+            addAttr("/user/yexz/"+filename,"displayName",filename);
+            addAttr("/user/yexz/"+filename,"qualifiedName",filename+"@csv_files");
+            Log.info("Added");
+        } catch (IOException exception){}
+
+
+
+        // Start the hook
+    }
+    public static void connectHdfs() throws IOException {
+        config = new Configuration();
+        config.set("fs.defaultFS", "hdfs://hadoopnamenode:9000");
+        FileSystem fileSystem = FileSystem.get(config);
+    }
+    public static void createDirectory(String pathToDir) throws IOException {
+        fileSystem = FileSystem.get(config);
+        String directoryName = pathToDir;
+        Path path = new Path(directoryName);
+        fileSystem.mkdirs(path);
+
+    }
+    public static void writeFileToHDFS(String fileName) throws IOException {
+        fileSystem = FileSystem.get(config);
+        //Create a path
+        Path hdfsWritePath = new Path("/user/yexz/" + fileName);
+        FSDataOutputStream fsDataOutputStream = fileSystem.create(hdfsWritePath,true);
+        BufferedWriter bufferedWriter = new BufferedWriter(new OutputStreamWriter(fsDataOutputStream, StandardCharsets.UTF_8));
+        bufferedWriter.write("Java API to write data in HDFS");
+        bufferedWriter.newLine();
+        bufferedWriter.close();
+        fileSystem.close();
+    }
+    public static void appendToHDFSFile(String fileName) throws IOException {
+        FileSystem fileSystem = FileSystem.get(config);
+        //Create a path
+        Path hdfsWritePath = new Path("/user/yexz/javareadwriteexample/" + fileName);
+
+        FSDataOutputStream fsDataOutputStream = fileSystem.append(hdfsWritePath);
+
+        BufferedWriter bufferedWriter = new BufferedWriter(new OutputStreamWriter(fsDataOutputStream,StandardCharsets.UTF_8));
+        bufferedWriter.write("Java API to append data in HDFS file");
+        bufferedWriter.newLine();
+        bufferedWriter.close();
+        fileSystem.close();
+    } // tekhdmch
+    public static void readFileFromHDFS(String fileName) throws IOException {
+
+        FileSystem fileSystem = FileSystem.get(config);
+        //Create a path
+        Path hdfsReadPath = new Path("/user/yexz/javareadwriteexample/" + fileName);
+        //Init input stream
+        FSDataInputStream inputStream = fileSystem.open(hdfsReadPath);
+        //Classical input stream usage
+        String out= IOUtils.toString(inputStream, "UTF-8");
+        System.out.println(out);
+        BufferedReader bufferedReader = new BufferedReader(
+                new InputStreamReader(inputStream, StandardCharsets.UTF_8));
+        String line = null;
+        while ((line=bufferedReader.readLine())!=null){
+            System.out.println(line);
+        }
+        inputStream.close();
+        fileSystem.close();
+    }
+    public static void checkExists() throws IOException {
+
+        FileSystem fileSystem = FileSystem.get(config);
+        String directoryName = "/user/yexz/javareadwriteexample/";
+        Path path = new Path(directoryName);
+        if(fileSystem.exists(path)){
+            System.out.println("File/Folder Exists : "+path.getName());
+        }else{
+            System.out.println("File/Folder does not Exists : "+path.getName());
+        }
+    }
+    public static void listMetadata() throws IOException{
+        FileSystem fs = FileSystem.get(config);
+
+// Path of the directory to list metadata for
+        Path directoryPath = new Path("/");
+
+// List all files and directories in the given directory
+        FileStatus[] fileStatuses = fs.listStatus(directoryPath);
+
+// Iterate over each file and directory and print its metadata
+        for (FileStatus fileStatus : fileStatuses) {
+            System.out.println("File/directory name: " + fileStatus.getPath().getName());
+            System.out.println("File/directory path: " + fileStatus.getPath());
+            System.out.println("File/directory size: " + fileStatus.getLen());
+            System.out.println("File/directory owner: " + fileStatus.getOwner());
+            System.out.println("File/directory group: " + fileStatus.getGroup());
+            System.out.println("File/directory permissions: " + fileStatus.getPermission());
+            System.out.println("File/directory modification time: " + new Date(fileStatus.getModificationTime()));
+            System.out.println("File/directory access time: " + new Date(fileStatus.getAccessTime()));
+            System.out.println("Is directory? " + fileStatus.isDirectory());
+            System.out.println("Is file? " + fileStatus.isFile());
+            System.out.println();
+        }
+
+// Close the file system
+        fs.close();
+    }
+
+
+    public static void addAttr(String filePath, String name, String value) throws IOException {
+        fileSystem = FileSystem.get(config);
+        //String filePath = "/user/yexz/test1";
+        Path path = new Path(filePath);
+        //FileStatus fileStatus = fileSystem.getFileStatus(path);
+        //name = "user.author";
+        //String value = "John Doe";
+        fileSystem.setXAttr(path, "user."+name, value.getBytes());
+    }
+    public static Map<String, byte[]> getAttr(String filePath) throws IOException {
+        fileSystem = FileSystem.get(config);
+        Path path = new Path(filePath);
+
+
+        Map<String, byte[]> xattrs = fileSystem.getXAttrs(path);
+       /* for (Map.Entry<String, byte[]> entry : xattrs.entrySet()) {
+            String xAttrName = entry.getKey();
+            byte[] xAttrValue = entry.getValue();
+            String encodedValue = new String(xAttrValue, StandardCharsets.UTF_8);
+            System.out.println("XAttr: " + xAttrName + " Value: " + encodedValue);
+        }*/
+
+        System.out.println(fileSystem.getFileStatus(path));
+        return xattrs;
+    }
+
+
+}
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -11,8 +11,7 @@ import org.apache.atlas.v1.model.instance.Referenceable;
 import org.apache.atlas.v1.model.notification.HookNotificationV1;
 import org.apache.atlas.model.notification.HookNotification;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 import static org.apache.atlas.hook.AtlasHook.getUser;
 import static org.apache.atlas.hook.AtlasHook.notifyEntities;
@@ -36,8 +35,6 @@ import org.apache.hadoop.security.UserGroupInformation;
 
 import java.io.*;
 import java.nio.charset.StandardCharsets;
-import java.util.Collections;
-import java.util.Date;
 
 
 public class Main extends AtlasHook{
@@ -45,135 +42,26 @@ public class Main extends AtlasHook{
     static Configuration config = null;
 
     public static void main(String[] args) throws IOException, MissingEventsException, InterruptedException {
-        connectHdfs();
+        HdfsY hdfsY = new HdfsY();
+        config = HdfsY.config;
+        fileSystem = FileSystem.get(config);
 
-        /*try {
-            createDirectory("/user/yexz/javaDir2");
-        } catch (IOException exception){}
-        try {
-            writeFileToHDFS("write_hdfs_example.txt");
-        } catch (IOException exception){}
-        try {
-            appendToHDFSFile("write_hdfs_example.txt");
-        } catch (IOException exception){}
-        try {
-            readFileFromHDFS("write_hdfs_example.txt");
-        } catch (IOException exception){}*/
-        //checkExists();
-
-        listMetadata();
-        notif();
-        //addAttr();
-        //getAttr();
 
         // Start the hook
-    }
-    public static void connectHdfs(){
-        config = new Configuration();
-        config.set("fs.defaultFS", "hdfs://hadoopnamenode:9000");
-    }
-    public static void createDirectory(String pathToDir) throws IOException {
-        fileSystem = FileSystem.get(config);
-        String directoryName = pathToDir;
-        Path path = new Path(directoryName);
-        fileSystem.mkdirs(path);
-
-    }
-    public static void writeFileToHDFS(String fileName) throws IOException {
-        FileSystem fileSystem = FileSystem.get(config);
-        //Create a path
-        Path hdfsWritePath = new Path("/user/yexz/javareadwriteexample/" + fileName);
-        FSDataOutputStream fsDataOutputStream = fileSystem.create(hdfsWritePath,true);
-        BufferedWriter bufferedWriter = new BufferedWriter(new OutputStreamWriter(fsDataOutputStream, StandardCharsets.UTF_8));
-        bufferedWriter.write("Java API to write data in HDFS");
-        bufferedWriter.newLine();
-        bufferedWriter.close();
-        fileSystem.close();
+        notif("csv_files");
     }
 
-    public static void appendToHDFSFile(String fileName) throws IOException {
-        FileSystem fileSystem = FileSystem.get(config);
-        //Create a path
-        Path hdfsWritePath = new Path("/user/yexz/javareadwriteexample/" + fileName);
 
-        FSDataOutputStream fsDataOutputStream = fileSystem.append(hdfsWritePath);
-
-        BufferedWriter bufferedWriter = new BufferedWriter(new OutputStreamWriter(fsDataOutputStream,StandardCharsets.UTF_8));
-        bufferedWriter.write("Java API to append data in HDFS file");
-        bufferedWriter.newLine();
-        bufferedWriter.close();
-        fileSystem.close();
-    } // tekhdmch
-
-    public static void readFileFromHDFS(String fileName) throws IOException {
-
-        FileSystem fileSystem = FileSystem.get(config);
-        //Create a path
-        Path hdfsReadPath = new Path("/user/yexz/javareadwriteexample/" + fileName);
-        //Init input stream
-        FSDataInputStream inputStream = fileSystem.open(hdfsReadPath);
-        //Classical input stream usage
-        String out= IOUtils.toString(inputStream, "UTF-8");
-        System.out.println(out);
-        BufferedReader bufferedReader = new BufferedReader(
-                new InputStreamReader(inputStream, StandardCharsets.UTF_8));
-        String line = null;
-        while ((line=bufferedReader.readLine())!=null){
-            System.out.println(line);
-        }
-        inputStream.close();
-        fileSystem.close();
-    }
-
-    public static void checkExists() throws IOException {
-
-        FileSystem fileSystem = FileSystem.get(config);
-        String directoryName = "/user/yexz/javareadwriteexample/";
-        Path path = new Path(directoryName);
-        if(fileSystem.exists(path)){
-            System.out.println("File/Folder Exists : "+path.getName());
-        }else{
-            System.out.println("File/Folder does not Exists : "+path.getName());
-        }
-    }
-
-    public static void listMetadata() throws IOException{
-        FileSystem fs = FileSystem.get(config);
-
-// Path of the directory to list metadata for
-        Path directoryPath = new Path("/");
-
-// List all files and directories in the given directory
-        FileStatus[] fileStatuses = fs.listStatus(directoryPath);
-
-// Iterate over each file and directory and print its metadata
-        for (FileStatus fileStatus : fileStatuses) {
-            System.out.println("File/directory name: " + fileStatus.getPath().getName());
-            System.out.println("File/directory path: " + fileStatus.getPath());
-            System.out.println("File/directory size: " + fileStatus.getLen());
-            System.out.println("File/directory owner: " + fileStatus.getOwner());
-            System.out.println("File/directory group: " + fileStatus.getGroup());
-            System.out.println("File/directory permissions: " + fileStatus.getPermission());
-            System.out.println("File/directory modification time: " + new Date(fileStatus.getModificationTime()));
-            System.out.println("File/directory access time: " + new Date(fileStatus.getAccessTime()));
-            System.out.println("Is directory? " + fileStatus.isDirectory());
-            System.out.println("Is file? " + fileStatus.isFile());
-            System.out.println();
-        }
-
-// Close the file system
-        fs.close();
-    }
-
-    public static void notif() throws IOException, MissingEventsException, InterruptedException {
-
-        fileSystem = FileSystem.get(config);
+    public static void notif(String TypeName) throws IOException, MissingEventsException, InterruptedException {
+        int i=0;
+        //fileSystem = FileSystem.get(config);
         HdfsAdmin admin = new HdfsAdmin(fileSystem.getUri(), config);
         DFSInotifyEventInputStream eventStream = admin.getInotifyEventStream();
         /**/
         List<HookNotification> messages = new ArrayList<>();
         /**/
         while (true) {
+            i = 0;
             EventBatch events = eventStream.take();
             for (Event event : events.getEvents()) {
                 System.out.println("event type = " + event.getEventType());
@@ -182,36 +70,32 @@ public class Main extends AtlasHook{
                     case CREATE:
                         Event.CreateEvent createEvent = (Event.CreateEvent) event;
                         System.out.println("  path = " + createEvent.getPath());
-                        List<Referenceable> entities = new ArrayList<>();
+
+
+
                         AtlasEntityDef entityDef = new AtlasEntityDef();
-
-// set the qualified name for the entity class
-                        entityDef.setName("");
-
-
+                        entityDef.setName(createEvent.getPath());
                         AtlasEntity cre = new AtlasEntity(entityDef);
-                        cre.setTypeName("DataSet");
-                        cre.setAttribute("displayName","myCompany");
-                        cre.setAttribute("owner","yexz");
-                        cre.setAttribute("name","myCompany");
-                        cre.setAttribute("qualifiedName","myCompany@DataSet");
-                        //Referenceable cre = new Referenceable();
-                        //cre.setTypeName("DataSet");
-                        //entities.add(cre);
-                        /**/
+                        cre.setTypeName(TypeName);
+                        Path path = new Path(createEvent.getPath());
+                        cre.setAttribute("owner",fileSystem.getFileStatus(path).getOwner());
+                        cre.setAttribute("path",fileSystem.getFileStatus(path).getPath());
+                        cre.setAttribute("ModificationTime",fileSystem.getFileStatus(path).getModificationTime());
 
+                        Map<String, byte[]> attrs = HdfsY.getAttr(createEvent.getPath());
 
+                        for (Map.Entry<String, byte[]> entry : attrs.entrySet()) {
+                            String xAttrName = entry.getKey().substring(5);
+                            byte[] xAttrValue = entry.getValue();
+                            String encodedValue = new String(xAttrValue, StandardCharsets.UTF_8);
+                            cre.setAttribute(xAttrName,encodedValue);
+
+                        }
 
 
                         messages.add(new EntityCreateRequestV2("yexz", new AtlasEntity.AtlasEntitiesWithExtInfo(cre)));
-                        //messages.add(new HookNotificationV1.EntityCreateRequest("yexz", entities));
-                        notifyEntities(messages, null,10,null);
+                        notifyEntities(messages, null,5,null);
                         /**/
-/**/
-
-
-
-/**/
                         break;
                     case RENAME:
                         Event.RenameEvent renameEvent= (Event.RenameEvent) event;
@@ -241,44 +125,10 @@ public class Main extends AtlasHook{
     }
 
 
-    public static void addAttr() throws IOException {
-        fileSystem = FileSystem.get(config);
-        String filePath = "/user/yexz/test1";
-        Path path = new Path(filePath);
-
-        String name = "user.author";
-        String value = "John Doe";
-
-        fileSystem.setXAttr(path, name, value.getBytes());
-    }
-    public static void getAttr() throws IOException {
-        fileSystem = FileSystem.get(config);
-        String filePath = "/user/yexz/test1";
-        Path path = new Path(filePath);
-
-        String name = "user.author";
-        String value = "John Doe";
-
-        System.out.println(fileSystem.getXAttrs(path));
-    }
-
     @Override
     public String getMessageSource() {
         return null;
     }
 
 
-
-
-/*
-    private List<Referenceable> createEntities(Event event, String user) {
-        List<Referenceable> entities = new ArrayList<>();
-
-        entities.add(FalconBridge.createClusterEntity((org.apache.falcon.entity.v0.cluster.Cluster) event.getEntity()));
-        entities.add();
-
-        //new AtlasHDFSCreateEvent();
-        ent
-            return entities;
-        }*/
 }
